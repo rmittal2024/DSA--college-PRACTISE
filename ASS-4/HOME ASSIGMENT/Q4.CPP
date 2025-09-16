4dbabf857fa16d2eff7ee18db8f6e72e1be0b63b
Raghav Mittal <rmittal_be24@thapar.edu>
2025-09-16 16:09:04 +0530

    UPDATE

---
+++ ASS-4/HOME ASSIGMENT/Q4.CPP
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+void firstNonRepeating(string str) {
+    queue<char> q;
+    int freq[26] = {0};
+    for (char ch : str) {
+        q.push(ch);
+        freq[ch - 'a']++;
+        while (!q.empty() && freq[q.front() - 'a'] > 1) {
+            q.pop();
+        }
+        if (q.empty())
+            cout << -1 << " ";
+        else
+            cout << q.front() << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    string input = "aabc"; 
+    firstNonRepeating(input);
+    return 0;
+}
